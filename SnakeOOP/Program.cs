63d52ec60ff46861dc6e1f307feb0e39e9ff0bdf
master <oskar.saarme04@gmail.com>
2022-02-07 18:28:53 +0200

    Week19

---
+++ SnakeOOP/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SnakeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int speed = 200;
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+            Point snakeTail = new Point(15, 15, 's');
+            Snake snake = new Snake(snakeTail, 3, Direction.RIGHT);
+            snake.Draw();
+
+            FoodGenerator foodGenerator = new FoodGenerator(80, 25, '@');
+            FoodGenerator foodGenerator2 = new FoodGenerator(80, 25, '@');
+            Point food = foodGenerator.GeneratorFood();
+            Point foodBad = foodGenerator2.GeneratorFoodBad();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            foodBad.Draw();
+            Console.ForegroundColor = ConsoleColor.White;
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodGenerator.GeneratorFood();
+                    food.Draw();
+                    score++;
+                    speed = speed - 7;
+                    WriteScore(score);
+                }
+                else
+                {
+                    snake.Move();
+                }
+                if (snake.EatBad(foodBad))
+                {
+                    foodBad = foodGenerator2.GeneratorFoodBad();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    foodBad.Draw();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    score--;
+                    speed = speed + 7;
+                    WriteScore(score);
+                }
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKeys(key.Key);
+                }
+
+                Thread.Sleep(speed);
+
+            }
+            string str_score = Convert.ToString(score);
+            WriteGameOver(str_score);
+
+            Console.ReadLine();
+        }
+        public static void WriteGameOver(string score)
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("======================", xOffset, yOffset++);
+            WriteText("===== GAME  OVER =====", xOffset, yOffset++);
+            WriteText("======================", xOffset, yOffset++);
+            WriteText($"=== YOUR SCORE : {score} ===", xOffset, yOffset++);
+            WriteText("======================", xOffset, yOffset++);
+        }
+        public static void WriteText(string text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+        public static void WriteScore(int score)
+        {
+            Console.SetCursorPosition(100, 13);
+            Console.WriteLine($"SCORE : {score}");
+        }
+    }
+}
---
+++ SnakeOOP/Snake.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeOOP
+{
+    class Snake : Figure
+    {
+        Random rnd = new Random();
+        int score = 16;
+        Direction direction;
+        public Snake(Point tail, int lenght, Direction _direction)
+        {
+            direction = _direction;
+            pointList = new List<Point>();
+            for(int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pointList.Add(p);
+            }
+        }
+        public void Move()
+        {
+            Point tail = pointList.First();
+            pointList.Remove(tail);
+            tail.Clear();
+
+            Point head = GetNextPoint();
+            pointList.Add(head);
+            if (score >= 5 && score < 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                head.Draw();
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+            else if (score >= 10 && score < 16)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                head.Draw();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (score >= 16)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                head.Draw();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                head.Draw();
+            }
+        }
+        public Point GetNextPoint()
+        {
+            Point head = pointList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+        public void HandleKeys(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+            {
+                direction = Direction.LEFT;
+            }
+            else if (key == ConsoleKey.RightArrow)
+            {
+                direction = Direction.RIGHT;
+            }
+            else if (key == ConsoleKey.UpArrow)
+            {
+                direction = Direction.UP;
+            }
+            else if (key == ConsoleKey.DownArrow)
+            {
+                direction = Direction.DOWN;
+            }
+        }
+        public bool Eat(Point food)
+        {
+            Point head = GetNextPoint();
+            if (head.IsHit(food))
+            {
+                food.symb = head.symb;
+                pointList.Add(food);
+                score++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool EatBad(Point foodBad)
+        {
+            Point head = GetNextPoint();
+            if (head.IsHit(foodBad))
+            {
+                foodBad.symb = head.symb;
+                score--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsHitTail()
+        {
+            Point head = pointList.Last();
+            for(int i = 0; i < pointList.Count - 2; i++)
+            {
+                if (head.IsHit(pointList[i]))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+    }
+}
